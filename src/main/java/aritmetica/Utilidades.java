726a3093eaebdd3536d98bf9a77190d1285564cf
serporion <138280818+serporion@users.noreply.github.com>
2024-02-02 10:09:43 +0100

    Update Utilidades.java

--- src/main/java/aritmetica/Utilidades.java
+++ src/main/java/aritmetica/Utilidades.java
@@ -10,27 +10,29 @@ public class Utilidades {
      
         int[] ordenados = new int[3];
 
-        if (num1 < num2 && num1 < num3) {
+        int[] ordenados = new int[3];
+
+        if (num1 <= num2 && num1 <= num3) {
             ordenados[0] = num1;
-            if (num2 < num3) {
+            if (num2 <= num3) {
                 ordenados[1] = num2;
                 ordenados[2] = num3;
             } else {
                 ordenados[1] = num3;
                 ordenados[2] = num2;
             }
-        } else if (num2 < num1 && num2 < num3) {
+        } else if (num2 <= num1 && num2 <= num3) {
             ordenados[0] = num2;
-            if (num1 < num3) {
+            if (num1 <= num3) {
                 ordenados[1] = num1;
                 ordenados[2] = num3;
             } else {
                 ordenados[1] = num3;
                 ordenados[2] = num1;
             }
-        } else if ( num3 < num2 && num3 < num1) {
+        } else if ( num3 <= num2 && num3 <= num1) {
             ordenados[0] = num3;
-            if (num2 < num1) {
+            if (num2 <= num1) {
                 ordenados[1] = num2;
                 ordenados[2] = num1;
             } else {
